33e727c6362376594ca6ddeda7a5d645d87f1003
Stefan Nägele <stefan.naegele@novatec-gmbh.de>
2019-05-17 13:44:36 +0200

    EAD-413: Add Spring Starter project

---
+++ r2dbc-spring/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.2.0.M3"
+	id("io.spring.dependency-management") version "0.6.0.RELEASE"
+	kotlin("jvm") version "1.3.31"
+	kotlin("plugin.spring") version "1.3.31"
+}
+
+group = "info.novatec"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+	maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
---
+++ r2dbc-spring/src/main/kotlin/info/novatec/r2dbcspring/R2dbcSpringApplication.kt
@@ -0,0 +1,11 @@
+package info.novatec.r2dbcspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class R2dbcSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<R2dbcSpringApplication>(*args)
+}
