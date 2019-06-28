3a8e156c19027ce9db3f5c2adfcb05ec2e65f548
Stefan Nägele <stefan.naegele@novatec-gmbh.de>
2019-06-28 11:42:07 +0200

    EAD-423: Implement basic todo list operations

--- r2dbc-spring/src/main/kotlin/info/novatec/r2dbc/spring/todo/ToDoListController.kt
+++ r2dbc-spring/src/main/kotlin/info/novatec/r2dbc/spring/todo/ToDoListController.kt
@@ -3,31 +3,22 @@ package info.novatec.r2dbc.spring.todo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
 @RequestMapping("/v1/todos")
-class ToDoListController @Autowired constructor(private val repository: ToDoRepository) {
+class ToDoListController(@Autowired private val repository: ToDoRepository) {
 
     @GetMapping
     fun getTodoList() = ResponseEntity.ok().body(repository.findAll())
 
     @GetMapping("/{todoId}")
-    fun getTodo(@PathVariable("todoId") todoId: Long) = ResponseEntity.ok(repository.findById(todoId))
+    fun getTodo(@PathVariable("todoId") todoId: String) = ResponseEntity.ok(repository.findById(todoId))
 
     @PostMapping
-    fun addToDoEntry(@RequestBody toDo: ToDoEntity): ResponseEntity.BodyBuilder {
-        val addedToDo = repository.save(toDo)
-        val location = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/v1/todos/{todoId}")
-                .buildAndExpand(addedToDo.id)
-                .toUri()
-
-        return ResponseEntity.created(location)
-    }
+    fun addTodo(@RequestBody toDo: ToDoEntity) = repository.save(toDo)
 
     @DeleteMapping("/{todoId}")
-    fun deleteEntry(@PathVariable("todoId") todoId: Long): ResponseEntity<Unit> {
+    fun deleteTodo(@PathVariable("todoId") todoId: String): ResponseEntity<Unit> {
         repository.deleteById(todoId)
         return ResponseEntity.noContent().build()
     }
--- r2dbc-spring/src/main/kotlin/info/novatec/r2dbc/spring/todo/ToDoRepository.kt
+++ r2dbc-spring/src/main/kotlin/info/novatec/r2dbc/spring/todo/ToDoRepository.kt
@@ -2,4 +2,4 @@ package info.novatec.r2dbc.spring.todo
 
 import org.springframework.data.repository.CrudRepository
 
-interface ToDoRepository : CrudRepository<ToDoEntity, Long>
+interface ToDoRepository : CrudRepository<ToDoEntity, String>
